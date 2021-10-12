3d779b1c29cbf2cf8b19747ce2fab0d884e70d06
Henrique Balardin <balardinh@gmail.com>
2021-10-12 20:57:00 -0300

    chore: create question model

---
+++ src/modules/questions/model/Question.ts
@@ -0,0 +1,13 @@
+import { v4 as uuidV4 } from 'uuid';
+
+class Question {
+  id: string;
+  title: string;
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) this.id = uuidV4();
+  }
+}
+
+export { Question };
