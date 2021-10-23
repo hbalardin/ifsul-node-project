597b0604f30a9f4de3aac8fb543989ff902af424
Henrique Balardin <balardinh@gmail.com>
2021-10-23 17:55:29 -0300

    chore (entities): update question model to typeorm entity

--- src/modules/questions/entities/Question.ts
+++ src/modules/questions/entities/Question.ts
@@ -1,12 +1,17 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn } from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 
+@Entity('questions')
 class Question {
+  @PrimaryColumn()
   id: string;
 
+  @Column()
   title: string;
 
   linked_answer_id?: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
