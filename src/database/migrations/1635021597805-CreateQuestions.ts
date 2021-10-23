d084510ca4ea608262f59d240383b711b1e073f4
Henrique Balardin <balardinh@gmail.com>
2021-10-23 17:50:14 -0300

    chore (migration): create questions table

---
+++ src/database/migrations/1635021597805-CreateQuestions.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateQuestions1635021597805 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'questions',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('questions');
+  }
+}
