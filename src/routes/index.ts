9b53f38fe82492bac47fc5f2702de5eb5b5bf11f
Henrique Balardin <balardinh@gmail.com>
2021-10-24 02:05:41 -0300

    chore: remove mocks

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,6 @@
 import { Router } from 'express';
 
 import { answersRoutes } from './answers.routes';
-// temp
-import { mocksRoutes } from './mocks.routes';
 import { questionsRoutes } from './questions.routes';
 import { registersRoutes } from './registers.routes';
 import { stepsRoutes } from './steps.routes';
@@ -13,7 +11,5 @@ router.use('/answers', answersRoutes);
 router.use('/questions', questionsRoutes);
 router.use('/registers', registersRoutes);
 router.use('/steps', stepsRoutes);
-
-router.use('/mocks', mocksRoutes);
 
 export { router };
