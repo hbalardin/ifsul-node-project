f8337dd10b930766b5b3aa67bbc8cd0d4877c569
Henrique Balardin <balardinh@gmail.com>
2021-10-31 15:22:10 -0300

    fix: conflicts

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import 'reflect-metadata';
+
+import cors from 'cors';
 import express from 'express';
 
 import { router } from './routes';
@@ -9,6 +11,7 @@ import './shared/container';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(router);
